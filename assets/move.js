dfad35111054db651609acfc7e437a832ca486f2
Natalie E. Ottolia <33526906+neonotfrommatrix@users.noreply.github.com>
2019-05-04 13:43:47 -0700

    Update move.js

--- assets/move.js
+++ assets/move.js
@@ -68,115 +68,137 @@ const Y_CORD = 1;
 var total_edge_pile = Math.floor(Math.random() * 15.99) + 15;
 var edge_pile = total_edge_pile - 6;
 
-
-while(edge_pile > 1)
-{
-    var current_highest = 0;
-    var start = [-1,-1];
-    var highest_target = [-1,-1];
-    var highest_return = [-1,-1];
-    var first_edge_flow = 0;
-    var second_edge_flow = 0;
-    
-    var vertex_count = vertices.length;
-    for(var vertex = 0; vertex < vertex_count; vertex++)
+function add_edge_pair(){
+    if(edge_pile > 1)
     {
-        var this_x = vertices[vertex][X_CORD];
-        var this_y = vertices[vertex][Y_CORD];
-        
-        var targets = [];
-        
-        //From the current vertex, check all 8 directions to find target vertices
-        for(var direction = 0; direction < DIRECTIONS.length; direction++)
+        var current_highest = 0;
+        var start = [-1,-1];
+        var highest_target = [-1,-1];
+        var highest_return = [-1,-1];
+        var first_edge_flow = 0;
+        var second_edge_flow = 0;
+        var vertex_count = vertices.length;
+        for(var vertex = 0; vertex < vertex_count; vertex++)
         {
-            var target_vertex_x = this_x + DIRECTIONS[direction][X_CORD];
-            var target_vertex_y = this_y + DIRECTIONS[direction][Y_CORD];
+            var this_x = vertices[vertex][X_CORD];
+            var this_y = vertices[vertex][Y_CORD];
             
-            //check the bounds
-            if(target_vertex_x > 9 || target_vertex_y > 9 || target_vertex_x < 0 || target_vertex_y < 0)
-                continue;
+            var targets = [];
             
-            //check if target_vertex is already in the vertices list
-            var found = false;
-            for(var vertix = 0; vertix < vertices.length; vertix++)
-            {
-                if(target_vertex_x == vertices[vertix][X_CORD] && target_vertex_y == vertices[vertix][Y_CORD])
-                {
-                    found = true;
-                    break;
-                }
-            }
-            if(found == false)
-            {
-                targets.push([target_vertex_x, target_vertex_y]);  
-            }
-        }
-        //Once all targets have been found, iterate through them to find candidate edge pairs
-        for(var target = 0; target < targets.length; target++)
-        {
-            var target_x = targets[target][X_CORD];
-            var target_y = targets[target][Y_CORD];
-            
-            //Once again, we iterate through all 8 directions to find return vertex candidates
+            //From the current vertex, check all 8 directions to find target vertices
             for(var direction = 0; direction < DIRECTIONS.length; direction++)
             {
-                var return_vertex_x = target_x + DIRECTIONS[direction][X_CORD];
-                var return_vertex_y = target_y + DIRECTIONS[direction][Y_CORD];
+                var target_vertex_x = this_x + DIRECTIONS[direction][X_CORD];
+                var target_vertex_y = this_y + DIRECTIONS[direction][Y_CORD];
                 
                 //check the bounds
-                if(return_vertex_x > 9 || return_vertex_y > 9 || return_vertex_x < 0 || return_vertex_y < 0)
+                if(target_vertex_x > 9 || target_vertex_y > 9 || target_vertex_x < 0 || target_vertex_y < 0)
                     continue;
                 
-                //Prevent a closed loop by not allowing the return vertex to equal the source vertex
-                if(return_vertex_x == this_x && return_vertex_y == this_y)
-                    continue;
-                
-                //The return vertex must be in the list of vertices, so continue if not
+                //check if target_vertex is already in the vertices list
                 var found = false;
-                for(var v = 0; v < vertices.length; v++)
+                for(var vertix = 0; vertix < vertices.length; vertix++)
                 {
-                    if(return_vertex_x == vertices[v][X_CORD] && return_vertex_y == vertices[v][Y_CORD])
+                    if(target_vertex_x == vertices[vertix][X_CORD] && target_vertex_y == vertices[vertix][Y_CORD])
                     {
                         found = true;
                         break;
                     }
                 }
-                if(found == true)
+                if(found == false)
                 {
-                    var target_flow = ((knightsBoard[this_x][this_y] + knightsBoard[target_x][target_y]) / 2);
-                    var return_flow = ((knightsBoard[target_x][target_y] + knightsBoard[return_vertex_x][return_vertex_y]) / 2);
-                    var max_flow = Math.min(target_flow, return_flow);
-                    if(max_flow > current_highest)
+                    targets.push([target_vertex_x, target_vertex_y]);  
+                }
+            }
+            //Once all targets have been found, iterate through them to find candidate edge pairs
+            for(var target = 0; target < targets.length; target++)
+            {
+                var target_x = targets[target][X_CORD];
+                var target_y = targets[target][Y_CORD];
+                
+                //Once again, we iterate through all 8 directions to find return vertex candidates
+                for(var direction = 0; direction < DIRECTIONS.length; direction++)
+                {
+                    var return_vertex_x = target_x + DIRECTIONS[direction][X_CORD];
+                    var return_vertex_y = target_y + DIRECTIONS[direction][Y_CORD];
+                    
+                    //check the bounds
+                    if(return_vertex_x > 9 || return_vertex_y > 9 || return_vertex_x < 0 || return_vertex_y < 0)
+                        continue;
+                    
+                    //Prevent a closed loop by not allowing the return vertex to equal the source vertex
+                    if(return_vertex_x == this_x && return_vertex_y == this_y)
+                        continue;
+                    
+                    //The return vertex must be in the list of vertices, so continue if not
+                    var found = false;
+                    for(var v = 0; v < vertices.length; v++)
                     {
-                        current_highest = max_flow;
-                        highest_target[X_CORD] = target_x;
-                        highest_target[Y_CORD] = target_y;
-                        highest_return[X_CORD] = return_vertex_x;
-                        highest_return[Y_CORD] = return_vertex_y;
-                        start[X_CORD] = this_x;
-                        start[Y_CORD] = this_y;
-                        first_edge_flow = target_flow;
-                        second_edge_flow = return_flow;
+                        if(return_vertex_x == vertices[v][X_CORD] && return_vertex_y == vertices[v][Y_CORD])
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if(found == true)
+                    {
+                        var heuristic = 1;
+                        var target_flow = ((knightsBoard[this_x][this_y] + knightsBoard[target_x][target_y]) / 2);
+                        var return_flow = ((knightsBoard[target_x][target_y] + knightsBoard[return_vertex_x][return_vertex_y]) / 2);
+                        var max_flow = Math.min(target_flow, return_flow);
+                        var left_edge =  [   [start[X_CORD],start[Y_CORD]],    [highest_target[X_CORD],highest_target[Y_CORD]],  first_edge_flow, 0 ];
+                        var right_edge = [   [highest_target[X_CORD],highest_target[Y_CORD]], [highest_return[X_CORD],highest_return[Y_CORD]], second_edge_flow, 0 ];
+                        edges.push(left_edge);
+                        edges.push(right_edge);
+                        vertices.push([highest_target[X_CORD],highest_target[Y_CORD]]);
+                        var increased_flow = Ford_Fulkerson(0,6);
+                        edges.pop();
+                        edges.pop();
+                        vertices.pop();
+                        if(highest_return[X_CORD] == 8 && highest_return[Y_CORD] == 7)
+                            heuristic++;
+                        if(start[X_CORD] == 1 && start[Y_CORD] == 2)
+                            heuristic++;
+                        
+                        var total_score = (max_flow * heuristic) + (increased_flow * 10);
+                        if( total_score > current_highest)
+                        {
+                            current_highest = total_score;
+                            highest_target[X_CORD] = target_x;
+                            highest_target[Y_CORD] = target_y;
+                            highest_return[X_CORD] = return_vertex_x;
+                            highest_return[Y_CORD] = return_vertex_y;
+                            start[X_CORD] = this_x;
+                            start[Y_CORD] = this_y;
+                            first_edge_flow = target_flow;
+                            second_edge_flow = return_flow;
+                        }
                     }
                 }
             }
         }
+        if(current_highest > 0)
+        {
+            var left_edge =  [   [start[X_CORD],start[Y_CORD]],    [highest_target[X_CORD],highest_target[Y_CORD]],  first_edge_flow, 0 ];
+            var right_edge = [   [highest_target[X_CORD],highest_target[Y_CORD]], [highest_return[X_CORD],highest_return[Y_CORD]], second_edge_flow, 0 ];
+            edges.push(left_edge);
+            edges.push(right_edge);
+            vertices.push([highest_target[X_CORD],highest_target[Y_CORD]]);
+        }
+        
+        edge_pile -= 2;
+        //Reset flows back to 0
+        for(var edge = 0; edge < edges.length; edge++)
+        {
+            edges[edge][CURR_FLOW] = 0;
+        }
     }
-    if(current_highest > 0)
-    {
-        var left_edge =  [   [start[X_CORD],start[Y_CORD]],    [highest_target[X_CORD],highest_target[Y_CORD]],  first_edge_flow, 0 ];
-        var right_edge = [   [highest_target[X_CORD],highest_target[Y_CORD]], [highest_return[X_CORD],highest_return[Y_CORD]], second_edge_flow, 0 ];
-        edges.push(left_edge);
-        edges.push(right_edge);
-        vertices.push([highest_target[X_CORD],highest_target[Y_CORD]]);
-    }
-    
-    edge_pile -= 2;
 }
+
 // ================ Breadth First Search ===============================
 
 //parents will be a list of vertex indexes that hold the coordinates of the parent node
-var parents = new Array(vertices.length);
+var parents = new Array(30);
 function BFS(sourceVertex, sinkVertex)
 {
     //queue consists of ints that represent the vertex index of the vertices array
@@ -265,6 +287,7 @@ function Ford_Fulkerson(sourceVertex, sinkVertex)
             }
         }
         //Once more, iterate through the path and increase the flow of each edge
+
         for(v = sinkVertex; v != sourceVertex; v = parents[v])
         {
             u = parents[v];
@@ -283,6 +306,7 @@ function Ford_Fulkerson(sourceVertex, sinkVertex)
                 }
             }                 
         }
+        
         max_flow += path_flow;
     }
     return max_flow;
